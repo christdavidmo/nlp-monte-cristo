41484da4952eaf56a05a8ecc03c4644c313d315e
christdavidmo <christdavidmondjot6@gmail.com>
2026-06-25 09:21:23 +0000

    Mise à jour des packages de tokenisation

--- tokenisation/tokenisation_nltk.py
+++ tokenisation/tokenisation_nltk.py
@@ -1,12 +1,57 @@
-import requests
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.util import bigrams
+from collections import Counter
 
 
 class TokenisationNltk:
 
     @classmethod
     def tokenise(text):
-
+        # ===> Tokenise le texte 
         tokens = word_tokenize(text,language='french')
         return tokens
-      
+    
+
+    @classmethod
+    def remove_stopwords(tokens):
+        # ===> Supprime les stopwords français.
+        stop_words = set(stopwords.words('french'))
+
+        token_filtre = [t for t in tokens if t.lower() not in  stop_words]
+
+        return token_filtre
+    
+
+    @staticmethod
+    def get_bigramme(tokens):
+        # ===> Affiche le nombre de bigramme
+        bigrammes = list(bigrams(tokens))
+        return bigrammes
+    
+    
+    @staticmethod
+    def get_nbr_token(tokens):
+        # ===> Affiche le nombre de tokens
+        compteur = Counter(tokens)
+        return compteur
+    
+
+    @staticmethod
+    def get_status(tokens):
+        # ===> Affiche les statistiques
+
+        total = len(tokens)
+        unique = len(set(tokens))
+        richesse_lexicale = unique / total if total > 0 else 0
+
+
+        stats ={
+            'total' : total ,
+            'unique' : unique ,
+            'richesse_lexicale' : richesse_lexicale
+        }
+
+        return stats
+
+
--- tokenisation/tokenisation_spacy.py
+++ tokenisation/tokenisation_spacy.py
@@ -1,4 +1,8 @@
 import spacy
+from nltk.corpus import stopwords
+from nltk.util import bigrams
+
+from collections import Counter
 
 class TokenisationSpacy:
 
@@ -9,6 +13,45 @@ class TokenisationSpacy:
 
         doc =NLP(text)
 
-        token_spacy = [token.text for token in doc]
+        tokens = [token.text for token in doc]
 
-        return token_spacy
+        return tokens
+    
+
+    @staticmethod
+    def Remove_Stopwords(tokens):
+
+        stop_words= set(stopwords.words('french'))
+        token_filtre = [t for t in tokens if t.lower() not in stop_words]
+        return token_filtre
+    
+
+    @staticmethod
+    def Get_Bigramme(tokens):
+
+        bigrammes = list(bigrams(tokens))
+        return bigrammes
+    
+
+    @staticmethod
+    def Get_Nbr_Token(tokens):
+        compteur = Counter(tokens)
+        return compteur
+    
+
+    @staticmethod
+    def get_status(tokens):
+        # ===> Affiche les statistiques
+
+        total = len(tokens)
+        unique = len(set(tokens))
+        richesse_lexicale = unique / total if total > 0 else 0
+
+
+        stats ={
+            'total' : total ,
+            'unique' : unique ,
+            'richesse_lexicale' : richesse_lexicale
+        }
+
+        return stats
